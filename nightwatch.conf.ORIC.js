b6e0bbc7b4286f5b3ff7d571eeef838e881a1270
oSinclair <46382241+osrmmv@users.noreply.github.com>
2019-01-18 09:28:17 +0100

    Update nightwatch.conf.ORIC.js

--- nightwatch.conf.ORIC.js
+++ nightwatch.conf.ORIC.js
@@ -1,7 +1,7 @@
 require('env2')('.env'); // optionally store youre Evironment Variables in .env
 const seleniumServer = require("selenium-server");
 const chromedriver = require("chromedriver");
-const { expect } = require('chai');
+const { expect } = require("chai");
 const SCREENSHOT_PATH = "./screenshots/";
 
 // we use a nightwatch.conf.js file so we can include comments and helper functions
